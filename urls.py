8826e485ffe251c4debbd29f240583c99cef5eea
Sumea Qadraku <86671439+sumeaqadraku@users.noreply.github.com>
2024-08-01 16:27:06 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,8 +1,6 @@
-
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('api.urls'))
+    path('', home)
 ]
